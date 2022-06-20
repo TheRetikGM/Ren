9bb1eab4d4a16bfb931086ea020ac9f1ff0df62f
TheRetikGM <theretikgm@gmail.com>
2022-06-20 23:42:55 +0200

    meson ImGui wrap

--- src/include/Ren/GameLauncher.h
+++ src/include/Ren/GameLauncher.h
@@ -2,7 +2,7 @@
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_glfw.h"
-#include "imgui_stdlib.h"
+#include "misc/cpp/imgui_stdlib.h"
 #include "GameCore.h"
 
 namespace Ren
